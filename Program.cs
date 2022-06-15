4b22dc0e43a35d7edbe33b24927e2981842cb3fe
Sebastien "Glaas" Decuyper <s.decuyper314@gmail.com>
2022-06-15 16:16:59 +0200

    text generation working

--- Program.cs
+++ Program.cs
@@ -5,12 +5,63 @@ using System.Text;
 using System.IO;
 
 
-//File[] files = Directory.GetFiles(@"C:\Users\glaas\Repositories\Web\RockPaperScissorsBot\SS", "*.jpg", SearchOption.AllDirectories);
-
-List<string> files = Directory.GetFiles(@"C:\Users\glaas\Repositories\Web\RockPaperScissorsBot\SS", "*.jpg", SearchOption.AllDirectories).ToList();
-
-for (int i = 0; i < files.Count; i++)
+Console.WriteLine("Hey!");
+Console.WriteLine("G to generate text, R to rename file");
+string answer = Console.ReadLine().ToUpper();
+const string prefix = "{\n\"origin\": [\"#alternatives#\"],\n\"alternatives\" : [" + "\"" + "{img https://raw.githubusercontent.com/Glaas/RockPaperScissorsBot/main/SS/1.jpg}\",";
+const string suffix = "]/}";
+switch (answer)
 {
-    System.IO.File.Move(files[i], i + ".jpg");
+    case "G":
+        GenerateText();
+        break;
+    case "R":
+        RenameFiles();
+        break;
+    default:
+        Console.WriteLine("Invalid input");
+        break;
+}
 
+void GenerateText()
+{
+    //create new TXT file
+    string path = @"C:\Users\glaas\Repositories\Web\RockPaperScissorsBot";
+    string fileName = "output.txt";
+    string fullPath = Path.Combine(path, fileName);
+
+
+    using (FileStream fs = File.Create(fullPath))
+    {
+
+    }
+
+    //write in file
+    using (StreamWriter sw = File.AppendText(fullPath))
+    {
+        sw.WriteLine(prefix);
+        var listOfFiles = Directory.GetFiles(@"C:\Users\glaas\Repositories\Web\RockPaperScissorsBot\SS", "*.jpg", SearchOption.AllDirectories).ToList();
+        for (int i = 2; i < listOfFiles.Count; i++)
+        {
+            if (i == listOfFiles.Count - 1)
+            {
+                sw.WriteLine("\"{img https://raw.githubusercontent.com/Glaas/RockPaperScissorsBot/main/SS/" + i + ".jpg" + "}\"]\n}");
+                break;
+            }
+            sw.WriteLine("\"{img https://raw.githubusercontent.com/Glaas/RockPaperScissorsBot/main/SS/" + i + ".jpg" + "} \",");
+        }
+
+        Console.WriteLine("Done!");
+    }
+}
+
+void RenameFiles()
+{
+    List<string> files = Directory.GetFiles(@"C:\Users\glaas\Repositories\Web\RockPaperScissorsBot\SS", "*.jpg", SearchOption.AllDirectories).ToList();
+
+    for (int i = 0; i < files.Count; i++)
+    {
+        System.IO.File.Move(files[i], i + ".jpg");
+        System.Console.WriteLine("Renamed " + files[i] + " to " + i + ".jpg");
+    }
 }
